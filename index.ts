05303d85cd1cc356e64e6c816d799bb810dfecb6
Dirk Arnez <smalldirkalex@gmail.com>
2021-11-12 01:39:57 +0800

    - update index.ts

--- index.ts
+++ index.ts
@@ -1,70 +1,83 @@
-class MyArray extends Array {
-  singleOrDefault = callback => {
-    var foundFirst = this.findIndex(callback);
-    if (foundFirst > -1) {
-      var foundSecond = 
-      this
-      .slice(foundFirst + 1)
-      .findIndex(callback);
-      
-      return foundSecond > -1 ? undefined : this[foundFirst];
-    }
-    return undefined;
-  }
+interface Order {
+    id: number
+    amount: number
+    country: string
 }
 
+class EnhancedArray<T> extends Array<T>{
+    protected parent?: EnhancedArray<T>;
+    private sql: string;
+
+    constructor(...items: T[]) {
+        super(...items);
+        this.sql = "";
+        this.parent = undefined;
+    }
+
+    GREATER_THAN(key: keyof T, targetValue: any): EnhancedArray<T> {
+        const filtered =  this.filter(value => value[key] > targetValue);
+        const retValue = new EnhancedArray<T>(...filtered);
+        retValue.parent = this;
+
+        if (!this.parent) {
+            this.sql = `select * from Orders where ${key} > ${targetValue}`;
+        } else {
+            this.sql = `and ${key} > ${targetValue}`;
+        }
+
+        return retValue;
+    }
+
+    EQUAL(key: keyof T, targetValue: any): EnhancedArray<T> {
+        const filtered =  this.filter(value => value[key] > targetValue);
+        const retValue = new EnhancedArray<T>(...filtered);
+        retValue.parent = this;
+        if (!this.parent) {
+            this.sql = `select * from Orders where ${key} = ${targetValue}`;
+        } else {
+            this.sql = `and ${key} = ${targetValue}`;
+        }
+
+        return retValue;
+    }
+
+    // singleOrDefault = callback => {
+    //     var foundFirst = this.findIndex(callback);
+    //     if (foundFirst > -1) {
+    //     var foundSecond = 
+    //     this
+    //     .slice(foundFirst + 1)
+    //     .findIndex(callback);
+        
+    //     return foundSecond > -1 ? undefined : this[foundFirst];
+    //     }
+    //     return undefined;
+    // }
+
+    toSQL(): string {
+        if (!!this.parent) {
+            const parentSQL = this.parent.toSQL();
+            const thisSQL = !!this.sql && this.sql.length > 0 ? " " + this.sql : "";
+            return this.parent.toSQL() + thisSQL;
+        }
+        return this.sql;
+    }
+}
+
+const orders = new EnhancedArray<Order>();
+orders.push( { id: 0, amount: 140, country: "USA"});
+orders.push( { id: 0, amount: 120, country: "AUS"});
+orders.push( { id: 0, amount: 50, country: "JPN"});
 
 
 console.log(
-  MyArray
-  .of(1, 2, 14, 15)
-  .singleOrDefault((element) => element > 13)
+    orders
+    .GREATER_THAN("amount", 100)
+    .GREATER_THAN("amount", 100)
+    .EQUAL("country", "AUS")
+    .EQUAL("country", "JPN")
+    .toSQL()
 );
+// "select * from Orders where amount > 100 and amount > 100 and country = AUS and country = JPN"
 
-
-function wrapped(arr) {
-  return {
-    _data: arr,
-    get data() { 
-      return this._data;
-    },
-    set data(newArr) {
-      this._data = newArr;
-    }
-  };
-}
-
-const warr = wrapped([1, 2, 14, 15]);
-warr.data = [1, 3];
-
-
-
-
-
-
-
-
-
-
-
-///////
-  interface Order {
-      id: number,
-      amount: number
-  }
-
-
-class EnhancedArray<T> extends Array<T>{
-    LARGER_THAN(key: keyof T, targetValue: any): string {
-        const filtered =  this.filter(item => item[key] > targetValue);
-        return `select * from Orders where ${key} > ${targetValue};`;
-    }
-}
-
-
-const orders = new EnhancedArray<Order>();
-orders.push( { id: 0, amount: 140});
-orders.push( { id: 0, amount: 50});
-
-// "select * from Orders where amount > 100;" 
-console.log(orders.LARGER_THAN("amount", 100))
+// select * from Orders where amount > 100 and amount > 100 and country = AUS and country = JPN
