330d2fb1487c116ada8cbdbe2bdb8843ab5e8815
Dirk Arnez <smalldirkalex@gmail.com>
2021-11-13 19:20:06 +0800

    - update index.ts

--- index.ts
+++ index.ts
@@ -29,20 +29,6 @@ function getColumnName(target: any, propertyKey: string) {
   return Reflect.getMetadata(formatMetadataKey, target, propertyKey);
 }
 
-
-@Entity("Orders")
-class Order {
-    @PrimaryGeneratedColumn("id")
-    id: number = NaN
-    
-    @Column("amount")
-    amount: number = NaN
-    
-    @Column("country")
-    country?: string
-}
- 
-
 class EnhancedArray<T> extends Array<T>{
     protected parent?: EnhancedArray<T>;
     private sql: string;
@@ -52,6 +38,7 @@ class EnhancedArray<T> extends Array<T>{
         this.sql = "";
         this.parent = undefined;
     }
+
 
     GREATER_THAN(key: keyof T, targetValue: any): EnhancedArray<T> {
         const filtered =  this.filter(value => value[key] > targetValue);
@@ -65,6 +52,71 @@ class EnhancedArray<T> extends Array<T>{
         }
 
         return retValue;
+    }
+//()
+    // create<K extends keyof T, G extends T>(ttt: (item: G) => G[keyof G], c: { new (): G }, assesor: (item: typeof c) => boolean): G {
+    //     const omg = new c() as G;
+
+    //     // type OptionsFlags<Type> = {
+    //     //     [Property in keyof Type]: boolean;
+    //     // };
+
+        
+    //     console.log(Object.keys(omg));
+    //     return omg;
+    // }
+
+    // LESS_THAN<K extends keyof T, G extends T>(temp: T[K], accesor: (fake: G) => G[K], targetValue: any): EnhancedArray<T> {
+    //     type Clone = {
+    //         [P in keyof T]: T[P];
+    //     };
+
+
+    
+
+    LESS_THAN<K extends keyof T>(accesor: (item: T) => T[K], targetValue: any): EnhancedArray<T> {
+    //   const i: T = this[0];
+    //    const t = accesor(i);
+    //     type valueof = T[keyof T];
+    // const aaaa: valueof = accesor(i);
+    //     console.log(aaaa);
+      
+        // const aaaa: (item: T, key: keyof T) => T[K] = (item) => {
+        //     console.log(key);
+        // }
+        
+        type TodoKeys = keyof T; // "id" | "text" | "due"
+        const target: TodoKeys = { } as TodoKeys;
+        
+        console.log(target);
+
+        // .forEach(key => {
+        //     Object.defineProperty(target, key, {
+        //         get() { console.log(`hi from ${key}`) },
+        //     });
+        // })
+     
+
+        //accesor(target);
+        
+
+
+        //debugger;
+        // console.log(accesor);
+        // aaaa(this[0]);
+
+        return this;
+        // const filtered =  this.filter(value => value[key] > targetValue);
+        // const retValue = new EnhancedArray<T>(...filtered);
+        // retValue.parent = this;
+
+        // if (!this.parent) {
+        //     this.sql = `select * from Orders where ${key} > ${targetValue}`;
+        // } else {
+        //     this.sql = `and ${key} > ${typeof targetValue == typeof "" ? `'${targetValue}'` : targetValue}`;
+        // }
+
+        // return retValue;
     }
 
     EQUAL(key: keyof T, targetValue: any): EnhancedArray<T> {
@@ -103,19 +155,56 @@ class EnhancedArray<T> extends Array<T>{
     }
 }
 
-const orders = new EnhancedArray<Order>();
-orders.push(...[
-    { id: 0, amount: 140, country: "USA"},
-    { id: 0, amount: 120, country: "AUS"},
-    { id: 0, amount: 50, country: "JPN"}
-]);
+@Entity("Orders")
+class Order {
+    @PrimaryGeneratedColumn("id")
+    id: number = NaN
+    
+    @Column("amount")
+    amount: number = NaN
+    
+    @Column("country")
+    country?: string
 
-console.log(
-    orders
-    .GREATER_THAN("amount", 100)
-    .GREATER_THAN("amount", 100)
-    .EQUAL("country", "AUS")
-    .EQUAL("country", "JPN")
-    .toSQL()
+    // private discounted: boolean = false;
+    
+    // set discounted(value: boolean) {
+    //     this._discounted = value;
+    // }
+}
+
+
+
+const orders = new EnhancedArray<Order>();
+orders.push(
+    { id: 1, amount: 140, country: "USA"},
+    { id: 2, amount: 120, country: "AUS"},
+    { id: 3, amount: 50, country: "JPN"}
 );
+
+// function prop<T extends object, K extends keyof T>(obj: T, key: K) {
+//   return obj[key];
+// }
+
+// type K1 = keyof Order; // "name" | "age" | "location"
+// const kkk: K1 = "name";
+
+//prop(new Order(). )
+// orders.create(Order, e => e.);
+orders.LESS_THAN(order => order.amount, 1);
+
+// (a => a.amount, 100);
+
+
+// best order => order.amount
+// mimic fake order => order.amount is "amount"
+
+// console.log(
+//     orders
+//     .GREATER_THAN("amount", 100)
+//     .EQUAL("country", "JPN")
+//     .toSQL()
+// );
+
+
 // "select * from Orders where amount > 100 and amount > 100 and country = AUS and country = JPN"
